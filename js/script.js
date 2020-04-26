e26fa7f907dafff68fe27af27283455add0d6d8e
Harsh <harshsharma2210>
2020-04-26 10:17:29 +0530

    jQeury Updated

--- js/script.js
+++ js/script.js
@@ -2,6 +2,16 @@ $(document).ready(function(event) {
 
     let scroll_top=$(".scroll-top");
     scroll_top.hide("slow");
-    $(window.scroll)
-
+    $(window).scroll(function(){
+        if($(window).scrollTop()>200){
+            scroll_top.show("slow");
+        }
+        else{
+            scroll_top.hide("slow");
+        }
+    });
+    scroll_top.click(function(){
+        $("html,body").animate({scrollTop:0},300);
+    })
+    
 });
